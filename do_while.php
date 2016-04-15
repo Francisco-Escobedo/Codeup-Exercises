846dac459d1063b4d6accbd295235dd744b99ab6
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-15 13:29:12 -0500

    Added do while loop that multiplies a variable by itself to a limit of 1,000,000, displaying the results at each increment.

--- do_while.php
+++ do_while.php
@@ -1,17 +1,26 @@
 <?php
 
-$n = 0;
+// $n = 0;
 
-// 0 to 100 by 2's 
+// // 0 to 100 by 2's 
+// do {
+//     echo "$n\n";
+//     $n+=2;
+// } while ($n<=100);
+
+// // 100 to -10 by 5's 
+// $u = 100;
+
+// do {
+//     echo "$u\n";
+//     $u-=5;
+// } while ($u>=-10);
+
+// 2 through 65536 by exponents
+
+$m = 2;
+
 do {
-    echo "$n\n";
-    $n+=2;
-} while ($n<=100);
-
-// 100 to -10 by 5's 
-$u = 100;
-
-do {
-    echo "$u\n";
-    $u-=5;
-} while ($u>=-10);
+    echo "$m\n";
+    $m*=$m;
+} while ($m<=1000000);

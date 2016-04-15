052ca3708412a40e14d3193071764ae886828271
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-15 10:53:12 -0500

    Added or statement to if and or exercise.

--- if_and_or.php
+++ if_and_or.php
@@ -13,7 +13,9 @@ if ($x < $y && $y < $z){
 // TODO:
 // If 0 is less than $x OR $x is less than 10
 // then echo the result as a sentence "0 is less than {$x} OR {$x} is less than 10".
-
+if (0<$x || $x<10){
+    echo "0 is less than {$x} OR {$x} is less than 10\n";
+}
 
 // TODO:
 // repeat the if statement for $y and $z.

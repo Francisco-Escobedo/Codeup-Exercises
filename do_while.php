bbf232fc860a714ae2d09103290b18a2d9bc2dbe
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-15 13:22:23 -0500

    Added second do while loop that increments variable from 100 to -10 and echos each increment by intervals of 5.

--- do_while.php
+++ do_while.php
@@ -2,8 +2,16 @@
 
 $n = 0;
 
+// 0 to 100 by 2's 
 do {
     echo "$n\n";
     $n+=2;
 } while ($n<=100);
 
+// 100 to -10 by 5's 
+$u = 100;
+
+do {
+    echo "$u\n";
+    $u-=5;
+} while ($u>=-10);

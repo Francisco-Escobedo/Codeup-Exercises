ab9bcb70ca1de42b1f8fdff41ab3d6d86f46e9dd
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-18 09:20:59 -0500

    Corrected do while loop to produce song up until 1 bottle to change grammar.

--- ninetynineBottles.php
+++ ninetynineBottles.php
@@ -1,10 +1,16 @@
 <?php
 
-echo "Tell me how many bottles are on the wall";
-
-$bottleCount = trim(fgets(STDIN));
+$bottleCount = 5;
 
 do {
-    echo "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer."
-    $bottleCount--;
-} while ($bottleCount>1);
+    usleep(200000);
+    echo "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\n";
+    usleep(200000);
+    $bottleCount-=1;
+    echo "Take one down and pass it around, $bottleCount bottles of beer on the wall\n";
+    usleep(300000);
+    if ($bottleCount==1){
+    echo "Take one down and pass it around, $bottleCount bottle of beer on the wall.\n";
+    }
+
+} while ($bottleCount > 0);

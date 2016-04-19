ec0984500ea6bdbbb1254bbb355ee3a9de8022be
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-19 15:36:24 -0500

    Validated arithmetic arguments that variables are numeric and in the division function the second variable may not be zero (to prevent division by zero).

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,5 @@
 <?php
-
+    
 // Global variables do not act the same here as they do in JavaScript. When outside a functions scope, a variable is its own entity.
 $a= 53;
 
@@ -7,38 +7,59 @@ $b= 44;
 
 function add($a, $b)
 {
-    return $a + $b;
+    if (is_numeric($a) && is_numeric($b)){
+        return $a + $b;
+    } else {
+        return "ERROR: Both arguments must be NUMBERS \n";
+    }
+    
 }
 
 function subtract($a, $b)
 {
-    return $a - $b;
+    if (is_numeric($a) && is_numeric($b)){
+        return $a - $b;
+    } else {
+        return "ERROR: Both arguments must be NUMBERS \n";
+    }
 }
 
 function multiply($a, $b)
 {
-    return $a * $b;
+    if (is_numeric($a) && is_numeric($b)){
+        return $a * $b;
+    } else {
+        return "ERROR: Both arguments must be NUMBERS \n";
+    }
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+    if (is_numeric($a) && is_numeric($b) && $b !== 0){
+        return $a / $b;
+    } else {
+        return "ERROR: Both arguments must be NUMBERS \n The second argument may NOT be ZERO\n";
+    }
 }
 
 function modulus($a, $b)
 {
-    return $a % $b;
+    if (is_numeric($a) && is_numeric($b)){
+        return $a % $b;
+    } else {
+        return "ERROR: Both arguments must be NUMBERS \n";
+    }
 }
 
 // Add code to test your functions here
 
 echo add(2, 5);
 echo "\n";
-echo subtract(2,5);
+echo subtract(2,"waffle");
 echo "\n";
 echo multiply(2,5);
 echo "\n";
-echo divide(100,5);
+echo divide(0, 25);
 echo "\n";
 echo modulus(25,4);
 echo "\n";

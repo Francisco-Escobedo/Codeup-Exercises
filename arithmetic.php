849433c96f4e8d51bd861c44fd8102041aee892c
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-04-19 14:27:23 -0500

    Entered parameters for functions to perform simple math functions and called each function with real numbers. Added a modulus operator to the existing list of functions.

---
+++ arithmetic.php
@@ -0,0 +1,40 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+}
+
+function modulus($a, $b)
+{
+    return $a % $b;
+}
+
+// Add code to test your functions here
+
+echo add(2, 5);
+echo "\n";
+echo subtract(2,5);
+echo "\n";
+echo multiply(2,5);
+echo "\n";
+echo divide(100,5);
+echo "\n";
+echo modulus(25,4);
+echo "\n";
+

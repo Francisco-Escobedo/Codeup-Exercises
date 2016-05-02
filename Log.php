d307cd123b9fe711869ce244f818a10820614990
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-02 14:10:40 -0500

    Added construct and destruct listeners in order to name file and handle to close file connection at destruction of class

--- Log.php
+++ Log.php
@@ -2,21 +2,31 @@
 
 class Log {
 
+    public function __construct($prefix='log') {
+        $date = date("Y-m-d");
+        $this->filename = "$prefix-$date.log";
+        $this->handle = fopen($this->filename, 'a');
+    }
+
+    public function __destruct() {
+        fclose($this->handle);
+    }
+
     public $filename;
 
+    public $handle;
+
     public function logMessage($logLevel, $message){
-        $handle = fopen($this->filename, 'a');
         $today = date("Y-m-d H:i:s");
-        fwrite($handle, $today . ' ' . $logLevel . ' ' . $message . PHP_EOL);
-        fclose($handle);
+        fwrite($this->handle, $today . ' ' . $logLevel . ' ' . $message . PHP_EOL);
     }
 
-    public function info(){
-        $this->logMessage('[INFO]', "This is an info message.");
+    public function info($message){
+        $this->logMessage('[INFO]', $message);
     }
 
-    public function error(){
-        $this->logMessage('[ERROR]', "This is an error message.");
+    public function error($message){
+        $this->logMessage('[ERROR]', $message);
     }
 
 }

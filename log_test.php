2dc12adfb3a49a9fc1fc22ead3250bd96cae9a98
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-02 15:18:09 -0500

    Commented out previous code and added construct function, append, and close functions to log.php to be referenced in log_test.php so that we may pass a file name, information to append, and a command to close the connection to the file that is being appended.

--- log_test.php
+++ log_test.php
@@ -2,6 +2,11 @@
 
 require 'Log.php';
 
-$test = new Log('cli');
-$test->info("This is an info message");
-$test->error("This is an error message");
+// $test = new Log('cli');
+// $test->info("This is an info message");
+// $test->error("This is an error message");
+
+
+$file = new File('file.txt');
+$file->append('Hello joshua!');
+$file->close();

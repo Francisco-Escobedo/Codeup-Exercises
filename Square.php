ce38f2afc95f1fe1a92dbe7e868ee63d53e70fda
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-17 11:20:34 -0500

    Update the constructor in Square to instead pass height on to the parent's constructor.

--- Square.php
+++ Square.php
@@ -5,9 +5,10 @@ class Square extends Rectangle
 
     public function __construct($height)
     {
-        $this->height = $height;
-        $this->width = $height;
+        parent::__construct($height, $height);
     }
+
+    public function area
 
 }
 

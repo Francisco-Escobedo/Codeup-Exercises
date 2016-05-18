11f66456d701a4da358a732b9bbfd25e75a8b1bb
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-18 12:10:22 -0500

    Created new file named model.php and a model_test to insure the code was working in Model.
    Created a Model class with
    
    An attributes property (array) that is not visible outside of the class
    A magic setter to create key/value pairs in the attributes array.
    A magic getter to retrieve values from the attributes array based on the key name, provided the key exists.

---
+++ model.php
@@ -0,0 +1,24 @@
+<?php
+
+class Model 
+{
+    private $attributes = [];
+
+    public function __set($name, $value)
+    {
+        $this->attributes[$name] = $value;
+        if(isset($this->attributes[$name])){
+            echo 'Attributes have been set'.PHP_EOL;
+        }
+    }
+
+    public  function __get($name)
+    {
+        if(array_key_exists($name, $this->attributes)){
+            echo "The attributes value for the attribute $name is: ";
+            echo $this->attributes[$name].PHP_EOL;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ model_test.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'model.php';
+
+$test = new Model();
+$test->flavor = 'Melon';
+
+$test->flavor;
+
